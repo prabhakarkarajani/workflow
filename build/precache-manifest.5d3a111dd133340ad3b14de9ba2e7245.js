26e32a95736af15aaca8d39c24b7ecbc6c305668
prabhakar karajani <prabhakarkarajani@prabhakars-MacBook-Pro.local>
2020-07-26 04:39:47 +0530

    serve build issue fixes added

---
+++ build/precache-manifest.5d3a111dd133340ad3b14de9ba2e7245.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583cd122e8d1af5622fad48d290559f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f62327373695f07153d8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "781ae4fd3d3e59db78bc",
+    "url": "/static/js/2.1224accf.chunk.js"
+  },
+  {
+    "revision": "edfd5a8be4ec1fc32e761a9220464431",
+    "url": "/static/js/2.1224accf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f62327373695f07153d8",
+    "url": "/static/js/main.32b1b0d8.chunk.js"
+  },
+  {
+    "revision": "ef9482a33432ddc2aee3",
+    "url": "/static/js/runtime-main.85514b1d.js"
+  }
+]);
